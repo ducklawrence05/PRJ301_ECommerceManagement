fbb1becec093017dbd72bfa4bd9f8fce42754932
tduc01234@gmail.com <tduc01234@gmail.com>
2025-06-15 22:26:45 +0700

    add response object

---
+++ src/java/response/ServiceResponse.java
@@ -0,0 +1,56 @@
+package response;
+
+public class ServiceResponse<T> {
+    private boolean success;
+    private String message;
+    private T data;
+
+    public ServiceResponse() {}
+
+    public ServiceResponse(boolean success, String message) {
+        this.success = success;
+        this.message = message;
+    }
+
+    public ServiceResponse(boolean success, String message, T data) {
+        this.success = success;
+        this.message = message;
+        this.data = data;
+    }
+
+    public static <T> ServiceResponse<T> success(String message, T data) {
+        return new ServiceResponse<>(true, message, data);
+    }
+
+    public static <T> ServiceResponse<T> success(String message) {
+        return new ServiceResponse<>(true, message, null);
+    }
+
+    public static <T> ServiceResponse<T> failure(String message) {
+        return new ServiceResponse<>(false, message, null);
+    }
+
+    public boolean isSuccess() {
+        return success;
+    }
+
+    public void setSuccess(boolean success) {
+        this.success = success;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+}
